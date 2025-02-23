42ff72e68b0eb564d68efa8a1b821ca31daab07a
lohith <lohithveepuri123@gmail.com>
2025-02-22 20:14:04 -0600

    updated

--- mergeIntervals.py
+++ mergeIntervals.py
@@ -1,17 +1,17 @@
-def mergeIntervals(intervals):
-    intervals.sort(key=lambda x:x[0])
+def mergeIntervals(l):
+    l.sort(key=lambda x:x[0])
     i=1
-    if len(intervals)==1 :
-        return intervals
-    while i<len(intervals) :
-        a=intervals[i-1]
-        b=intervals[i]
+    if len(l)==1 :
+        return l
+    while i<len(l) :
+        a=l[i-1]
+        b=l[i]
         if a[1]>=b[0] :
             if a[1]>b[1] :
-                intervals=intervals[:i-1]+[[a[0],a[1]]]+intervals[i+1:]
+                l=l[:i-1]+[[a[0],a[1]]]+l[i+1:]
             else :
-                intervals=intervals[:i-1]+[[a[0],b[1]]]+intervals[i+1:]
+                l=l[:i-1]+[[a[0],b[1]]]+l[i+1:]
         else :
             i+=1
-    return intervals
+    return l
 print(mergeIntervals([[1,3],[2,6],[8,10],[15,18]]))
